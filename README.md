f99449f81efbb99c07c160a17cf51dcca0b262c6
Thangvu21 <vuducthang212004@gmail.com>
2023-12-11 22:20:14 +0700

    write Readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Project-OOP
-Hel
+Hello đây là dự án của nhóm chúng mình làm về từ điển dưới đây là video demo
+<https://www.youtube.com/watch?v=1kVzPvKppgg>
+
--- src/main/java/com/example/demoDB/Main.java
+++ src/main/java/com/example/demoDB/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
@@ -14,17 +15,18 @@ import javafx.stage.Stage;
 import org.w3c.dom.Text;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main.fxml")));
-
         stage.setTitle("Dictionary");
-
+        URL url = Main.class.getResource("/Image/dictionary.png");
+        Image image = new Image(String.valueOf(url));
         Scene scene = new Scene(root, 850, 475);
-
+        stage.getIcons().add(image);
         stage.setScene(scene);
         stage.show();
     }
